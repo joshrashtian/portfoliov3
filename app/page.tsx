3adeae5758edc40c68ecc3966acf2b1dd7d46f4c
Joshua Rashtian <joshrashtian1@gmail.com>
2025-11-29 12:52:21 -1000

    Update package.json to include react-parallax-tilt dependency and enhance Home component layout with new header and tilt effects

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import { useRef } from "react";
 import { useEffect } from "react";
 import { motion } from "motion/react";
 import About from "./(components)/homepage/about";
+import Tilt from "react-parallax-tilt";
 
 export default function Home() {
   const sectionRefMap = useRef<{ [key: string]: HTMLDivElement | null }>({
@@ -31,7 +32,7 @@ export default function Home() {
     <div className="flex min-h-screen items-center justify-center font-lexend dark:bg-black">
       <main className="w-full max-w-7xl">
         <motion.div
-          className="flex min-h-screen relative  flex-col items-center justify-end py-32 px-16 bg-white dark:bg-black sm:items-start"
+          className="flex min-h-screen h-full relative  flex-col items-center justify-start py-16 px-16 bg-white dark:bg-black sm:items-start"
           ref={(el) => assignRef("home", el as HTMLDivElement)}
         >
           <motion.div
@@ -40,17 +41,26 @@ export default function Home() {
             transition={{ duration: 3, type: "spring" }}
             className="absolute z-50 left-0 bottom-24 w-2xl max-w-2xl h-2 bg-orange-400"
           />
-          <motion.h1
-            initial={{ opacity: 0, y: 100 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 1, delay: 3 }}
-            className="text-9xl absolute left-0 bottom-24 max-w-2xl font-bold"
-          >
-            Joshua Rashtian.
-          </motion.h1>
-          <div className="absolute right-0 text-right top-8">
-            <h2 className="text-2xl font-bold">Los Angeles, CA</h2>
-            <h3 className="text-lg text-gray-500">Software Engineer</h3>
+          <header className="flex flex-row w-full items-center justify-between">
+            <motion.h1 className="text-4xl max-w-2xl font-bold">
+              Joshua Rashtian.
+            </motion.h1>
+            <div className="mt-8 text-right">
+              <h2 className="text-2xl font-bold">Los Angeles, CA</h2>
+              <h3 className="text-lg text-gray-500">Software Engineer</h3>
+            </div>
+          </header>
+          <p>Embracing the revolution, whether it be in code and philosophy.</p>
+          <div className="grid grid-cols-3 w-full h-full gap-4">
+            <Tilt>
+              <div className="col-span-1 bg-gray-100 dark:bg-gray-800 rounded-lg p-4"></div>
+            </Tilt>
+            <Tilt>
+              <div className="col-span-1 bg-gray-100 dark:bg-gray-800 rounded-lg p-4"></div>
+            </Tilt>
+            <Tilt>
+              <div className="col-span-1 bg-gray-100 dark:bg-gray-800 rounded-lg p-4"></div>
+            </Tilt>
           </div>
         </motion.div>
         <div
